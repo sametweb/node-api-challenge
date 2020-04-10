370a3f75ec180da056bb5fbde3f82058b93c0528
sametweb <sametmutevellioglu@gmail.com>
2020-04-10 10:22:19 -0700

    React app router implementation

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,82 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  const [projects, setProjects] = useState([]);
+  return (
+    <Router>
+      <div className="App">
+        <Route path="/" exact component={Projects} />
+        <Route path="/:id" component={ProjectDetails} />
+      </div>
+    </Router>
+  );
+}
 
-  const fetchActions = id => {
+const ProjectDetails = (props) => {
+  const [singleProjectActions, setSingleProjectActions] = useState([]);
+  const { id } = props.match.params;
+  const project = props.location.state;
+
+  console.log(props);
+
+  const fetchActions = (id) => {
     axios
       .get(`https://samsprojects.herokuapp.com/projects/${id}/actions`)
-      .then(res => console.log(res.data));
+      .then((res) => setSingleProjectActions(res.data))
+      .catch((err) => console.log(err));
   };
+
+  useEffect(() => {
+    fetchActions(id);
+  }, [id]);
+
+  return (
+    <div
+      key={project.id}
+      className={`project${project.completed ? " completed" : ""} `}
+    >
+      <h3>{project.name}</h3>
+      <p>{project.description}</p>
+      <ul>
+        {singleProjectActions.length > 0 ? (
+          singleProjectActions.map((item) => (
+            <li key={item.id}>{item.description}</li>
+          ))
+        ) : (
+          <small>
+            <i>No action found.</i>
+          </small>
+        )}
+      </ul>
+    </div>
+  );
+};
+
+const Projects = (props) => {
+  const [projects, setProjects] = useState([]);
 
   useEffect(() => {
     axios
       .get("https://samsprojects.herokuapp.com/projects")
-      .then(res => setProjects(res.data))
-      .catch(error => console.log(error));
+      .then((res) => setProjects(res.data))
+      .catch((error) => console.log(error));
   }, []);
 
-  useEffect(() => {
-    projects.forEach(project => fetchActions(project.id));
-  }, [projects]);
-
-  return (
-    <div className="App">
-      {projects.map(project => {
-        return (
-          <div
-            key={project.id}
-            className={`project ${project.completed && "completed"}`}
-          >
-            <h3>{project.name}</h3>
-            <p>{project.description}</p>
-          </div>
-        );
-      })}
-    </div>
-  );
-}
+  return projects.map((project) => {
+    return (
+      <Link to={{ pathname: `/${project.id}`, state: project }}>
+        <div
+          key={project.id}
+          className={`project${project.completed ? " completed" : ""} `}
+        >
+          <h3>{project.name}</h3>
+          <p>{project.description}</p>
+        </div>
+      </Link>
+    );
+  });
+};
 
 export default App;
